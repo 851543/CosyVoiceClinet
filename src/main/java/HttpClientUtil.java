05e183de36d5661a5e1e36635bb2525a68f7fa2f
小气大方的阿俊 <130641570+851543@users.noreply.github.com>
2025-02-08 19:47:23 +0800

    更新新写法

--- src/main/java/HttpClientUtil.java
+++ src/main/java/HttpClientUtil.java
@@ -1,5 +1,6 @@
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
+import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpEntity;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.config.RequestConfig;
@@ -10,7 +11,12 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.entity.ContentType;
 import org.apache.http.entity.StringEntity;
+import org.apache.http.entity.mime.HttpMultipartMode;
+import org.apache.http.entity.mime.MultipartEntity;
 import org.apache.http.entity.mime.MultipartEntityBuilder;
+import org.apache.http.entity.mime.content.ByteArrayBody;
+import org.apache.http.entity.mime.content.FileBody;
+import org.apache.http.entity.mime.content.StringBody;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicNameValuePair;
@@ -19,6 +25,7 @@ import org.springframework.data.repository.init.ResourceReader;
 
 import java.io.*;
 import java.net.URI;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -273,6 +280,73 @@ public class HttpClientUtil {
             HttpEntity multipart = builder.build();
 
             httpPost.setEntity(multipart);
+            httpPost.setConfig(builderRequestConfig());
+
+            // 执行http请求
+            response = httpClient.execute(httpPost);
+
+            HttpEntity responseEntity = response.getEntity();
+
+            if (responseEntity != null) {
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                responseEntity.writeTo(baos);
+                wavByte = baos.toByteArray();
+            }
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            try {
+                response.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return wavByte;
+    }
+
+    /**
+     * 发送POST方式请求
+     *
+     * @param url
+     * @param paramMap
+     * @param promptWav
+     * @return
+     * @throws IOException
+     */
+    public static byte[] doPostBytes(String url, Map<String, String> paramMap, String promptWav) throws IOException {
+        // 创建Httpclient对象
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        CloseableHttpResponse response = null;
+        byte[] wavByte = null;
+
+        try {
+            // 创建Http Post请求
+            HttpPost httpPost = new HttpPost(url);
+
+            // 构建multipart请求体
+            MultipartEntity entity = new MultipartEntity(HttpMultipartMode.BROWSER_COMPATIBLE);
+
+            // 添加表单参数
+            if (paramMap != null) {
+                for (Map.Entry<String, String> param : paramMap.entrySet()) {
+                    StringBody contentBody = new StringBody(param.getValue(),
+                            "text/plain", Charset.forName("UTF-8"));
+                    entity.addPart(param.getKey(), contentBody);
+                }
+            }
+
+            // 添加音频文件
+            if (promptWav != null) {
+                // 添加文件
+                ByteArrayBody audioBody = new ByteArrayBody(
+                        IOUtils.toByteArray(ResourceReader.class.getClassLoader().getResourceAsStream(promptWav)),
+                        ContentType.APPLICATION_OCTET_STREAM,
+                        "prompt_wav"
+                );
+                entity.addPart("prompt_wav", audioBody);
+            }
+
+            httpPost.setEntity(entity);
             httpPost.setConfig(builderRequestConfig());
 
             // 执行http请求
